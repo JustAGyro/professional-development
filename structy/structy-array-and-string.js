181155fcaab55c403e0fb9e528f83e8b79495729
Kamran <kamran.zq@gmail.com>
2024-02-13 23:52:15 -0600

    adding anagrams solution

--- structy/structy-array-and-string.js
+++ structy/structy-array-and-string.js
@@ -52,3 +52,41 @@ const compress = (s) => {
 
   return compressed;
 };
+
+const anagrams = (s1, s2) => {
+  // todo
+  const objS1 = {};
+  const objS2 = {};
+
+  if (s1.length != s2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < s1.length; i++) {
+    let index = s1[i];
+    if (!objS1[index]) {
+      objS1[index] = 1;
+    }
+    if (objS1[index]) {
+      objS1[index] += 1;
+    }
+  }
+
+  for (let i = 0; i < s2.length; i++) {
+    let index = s2[i];
+    if (!objS2[index]) {
+      objS2[index] = 1;
+    }
+    if (objS2[index]) {
+      objS2[index] += 1;
+    }
+  }
+
+  for (key in objS2) {
+    if (objS2[key] != objS1[key]) {
+      return false;
+    }
+  }
+
+  return true;
+};
