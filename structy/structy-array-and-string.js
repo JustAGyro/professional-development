79678f32c6e68cf891e06eed45df0c466cd0369c
Kamran <kamran.zq@gmail.com>
2024-02-14 00:22:10 -0600

    added bad pairsum solution

--- structy/structy-array-and-string.js
+++ structy/structy-array-and-string.js
@@ -116,3 +116,16 @@ const mostFrequentChar = (s) => {
 
   return letter;
 };
+
+//O(n2)
+const pairSum = (numbers, targetSum) => {
+  // todo
+  for (let i = 0; i < numbers.length; i++) {
+    for (let j = 1; j < numbers.length; j++) {
+      let num1 = numbers[i];
+      let num2 = numbers[j];
+
+      if (num1 + num2 === targetSum) return [i, j];
+    }
+  }
+};
