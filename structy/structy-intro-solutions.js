91495c53c4cbcd8d4ac1ce6c48a904be75071cda
Kamran <kamran.zq@gmail.com>
2024-02-13 23:27:25 -0600

    adding compress

--- structy/structy-intro-solutions.js
+++ structy/structy-intro-solutions.js
@@ -63,3 +63,31 @@ const uncompress = (s) => {
   }
   return uncompressed;
 };
+
+const compress = (s) => {
+  // todo
+  let compressed = '';
+  let i = 0;
+  let j = 1;
+
+  while (j <= s.length) {
+    let cIndex = s[i];
+    let nIndex = s[j];
+
+    if (cIndex != nIndex) {
+      let sliced = s.slice(i, j);
+
+      if (sliced.length > 1) {
+        compressed += sliced.length + cIndex;
+      } else if (sliced.length === 1) {
+        compressed += cIndex;
+      }
+
+      i = j;
+    }
+
+    j++;
+  }
+
+  return compressed;
+};
