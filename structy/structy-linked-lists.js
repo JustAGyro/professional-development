c5db97c0124f896405fcb91d19bbba7b0a9d0363
Kamran <kamran.zq@gmail.com>
2024-02-20 17:57:22 -0600

    adding zipperlists

--- structy/structy-linked-lists.js
+++ structy/structy-linked-lists.js
@@ -72,3 +72,28 @@ const reverseList = (head) => {
 
   return prev;
 };
+
+const zipperLists = (head1, head2) => {
+  // todo
+  let count = 0;
+  let tail = head1;
+  let current1 = head1.next;
+  let current2 = head2;
+
+  while (current1 !== null && current2 !== null) {
+    if (count % 2 === 0) {
+      tail.next = current2;
+      current2 = current2.next;
+    } else {
+      tail.next = current1;
+      current1 = current1.next;
+    }
+    tail = tail.next;
+    count += 1;
+  }
+
+  if (current1 != null) tail.next = current1;
+  if (current2 != null) tail.next = current2;
+
+  return head1;
+};
