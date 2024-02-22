79577364690c4d6c82bd9530349a3b1064d416d9
Kamran <kamran.zq@gmail.com>
2024-02-22 11:12:22 -0600

    adding better solution to addlists

--- structy/structy-linked-lists.js
+++ structy/structy-linked-lists.js
@@ -267,3 +267,30 @@ const addLists = (head1, head2) => {
 
   return dummyHead.next;
 };
+
+const addLists2 = (head1, head2) => {
+  // todo
+  let current1 = head1;
+  let current2 = head2;
+  let carry = 0;
+
+  const dummyHead = new Node(0);
+  let tail = dummyHead;
+
+  while (current1 !== null || current2 !== null || carry === 1) {
+    let val1 = current1 === null ? 0 : current1.val;
+    let val2 = current2 === null ? 0 : current2.val;
+
+    let sum = val1 + val2 + carry;
+    carry = sum > 9 ? 1 : 0;
+    let digit = sum % 10;
+
+    if (current1 !== null) current1 = current1.next;
+    if (current2 !== null) current2 = current2.next;
+
+    tail.next = new Node(digit);
+    tail = tail.next;
+  }
+
+  return dummyHead.next;
+};
