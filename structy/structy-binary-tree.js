510090e47a9f2391f23a946e4155e2d8417a77bd
Kamran <kamran.zq@gmail.com>
2024-02-26 23:33:51 -0600

    adding depth first solution

--- structy/structy-binary-tree.js
+++ structy/structy-binary-tree.js
@@ -1,1 +1,20 @@
 // Binary tree solutions
+const depthFirstValues = (root) => {
+  // todo
+  let result = [];
+  let stack = [root];
+
+  if (root === null) return [];
+  let current = root;
+
+  while (stack.length > 0) {
+    current = stack.pop();
+
+    if (current.right != null) stack.push(current.right);
+    if (current.left != null) stack.push(current.left);
+
+    result.push(current.val);
+  }
+
+  return result;
+};
