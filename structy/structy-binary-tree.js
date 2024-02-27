81734b88b5aa0961caccaa51605084b44d26b507
Kamran <kamran.zq@gmail.com>
2024-02-27 00:13:05 -0600

    adding treesumdepth iterative

--- structy/structy-binary-tree.js
+++ structy/structy-binary-tree.js
@@ -43,3 +43,20 @@ const breadthFirstValues = (root) => {
 
   return result;
 };
+
+const treeSumDepthIterative = (root) => {
+  // todo
+  let sum = 0;
+
+  if (root === null) return 0;
+  let stack = [root];
+
+  while (stack.length > 0) {
+    const current = stack.pop();
+    sum += current.val;
+
+    if (current.right) stack.push(current.right);
+    if (current.left) stack.push(current.left);
+  }
+  return sum;
+};
