bfe3ef093b4fc341aa4a5938834e87dc9790e188
Kamran <kamran.zq@gmail.com>
2024-02-29 10:37:01 -0600

    adding pathfinder

--- structy/structy-binary-tree.js
+++ structy/structy-binary-tree.js
@@ -130,3 +130,21 @@ const maxPathSum = (root) => {
 
   return root.val + maxChildSum;
 };
+
+const pathFinder = (root, target) => {
+  if (root === null) return null;
+  if (root.val === target) return [root.val];
+
+  const leftPath = pathFinder(root.left, target);
+  const rightPath = pathFinder(root.right, target);
+
+  if (leftPath !== null) {
+    return [root.val, ...leftPath];
+  }
+
+  if (rightPath !== null) {
+    return [root.val, ...rightPath];
+  }
+
+  return null;
+};
