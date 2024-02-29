a653e6f64959cb098315ea203babbd66712af7d9
Kamran <kamran.zq@gmail.com>
2024-02-28 21:39:34 -0600

    adding treeminvalue

--- structy/structy-binary-tree.js
+++ structy/structy-binary-tree.js
@@ -103,3 +103,20 @@ const treeIncludes = (root, target) => {
 
   return false;
 };
+
+const treeMinValue = (root) => {
+  // todo
+  let min = root.val;
+  let stack = [root];
+
+  while (stack.length > 0) {
+    const current = stack.pop();
+
+    if (current.right) stack.push(current.right);
+    if (current.left) stack.push(current.left);
+
+    if (current.val < min) min = current.val;
+  }
+
+  return min;
+};
