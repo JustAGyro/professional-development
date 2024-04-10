2b2722b6ba37c8bd93b1809ba2fff626da8ea6cc
Kamran <kamran.zq@gmail.com>
2024-04-10 00:23:29 -0500

    solution 1 done

---
+++ hacker-rank-solutions/week-1.js
@@ -0,0 +1,26 @@
+function plusMinus(arr) {
+  const length = arr.length;
+
+  let positive = [];
+  let negative = [];
+  let zero = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    let current = arr[i];
+    if (current < 0) {
+      negative.push(current);
+    } else if (current > 0) {
+      positive.push(current);
+    } else {
+      zero.push(current);
+    }
+  }
+
+  const pRatio = (positive.length / length).toFixed(5);
+  const nRatio = (negative.length / length).toFixed(5);
+  const zRatio = (zero.length / length).toFixed(5);
+
+  console.log(pRatio);
+  console.log(nRatio);
+  console.log(zRatio);
+}
