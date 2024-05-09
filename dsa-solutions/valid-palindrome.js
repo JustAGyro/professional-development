12a256b2aae4b478d47f561998e0fa1c5694a62c
Kamran <kamran.zq@gmail.com>
2024-05-08 22:56:18 -0500

    adding solution

---
+++ dsa-solutions/valid-palindrome.js
@@ -0,0 +1,33 @@
+var validPalindrome = function (s) {
+  let left = 0;
+  let right = s.length - 1;
+
+  while (left < right) {
+    if (s.charAt(left) === s.charAt(right)) {
+      left++;
+      right--;
+    }
+    if (s.charAt(left) !== s.charAt(right)) {
+      return (
+        validPalindromeChecker(s, left + 1, right) ||
+        validPalindromeChecker(s, left, right - 1)
+      );
+    }
+  }
+  return true;
+};
+
+const validPalindromeChecker = (s, i, j) => {
+  let left = i;
+  let right = j;
+  while (left < right) {
+    if (s.charAt(i) === s.charAt(j)) {
+      left++;
+      right--;
+    }
+    if (s.charAt(i) !== s.charAt(j)) {
+      return false;
+    }
+  }
+  return true;
+};
